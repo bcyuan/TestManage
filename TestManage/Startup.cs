0876b1ec771889be0af0411c55d595640dad2251
bcyuan <www2024@163.com>
2017-12-04 13:24:19 +0800

    1

---
+++ TestManage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestManage.Startup))]
+namespace TestManage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
