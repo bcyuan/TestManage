69e42dee063662ce25ae2416d74a56dc16a38dbf
bcyuan <www2024@163.com>
2017-12-05 17:19:01 +0800

    2017年12月5日

---
+++ TestManage/Areas/TestManage/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestManage.Areas.TestManage.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: TestManage/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
---
+++ TestManage/Areas/TestManage/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestManage.Areas.TestManage.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: TestManage/Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
